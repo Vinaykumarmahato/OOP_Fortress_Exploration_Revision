454d62cdb076780d1628389b80b26f1d0ab2741d
Vinay kumar <vinaykumar860964@gmail.com>
2024-03-01 11:54:33 +0530

    updated

---
+++ 🧬 Inheritance/MoreOnInheritance.java
@@ -0,0 +1,27 @@
+class human {
+
+    public void Sleep(){
+
+        System.out.println("Human Need Good Sleep");
+
+        int age=30;
+    }
+
+}
+
+/**
+ *  Student
+ */
+ class  Student extends human{
+
+
+
+    
+}
+
+    
+public class MoreOnInheritance {
+    public static void main(String[] args) {
+        human inf=new human();
+    }
+}
