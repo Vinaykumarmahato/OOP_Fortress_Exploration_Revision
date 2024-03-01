6f98fee344812e7647898723f5a8ac1b06e0524a
Vinay kumar <vinaykumar860964@gmail.com>
2024-03-01 11:32:14 +0530

    Updated

---
+++ 🧬 Inheritance/Solution1.java
@@ -0,0 +1,28 @@
+
+ class Human{
+
+    void sleep(){
+
+        System.out.println("Human Need Good Sleep");
+    }
+
+}
+
+class Student extends Human{
+
+
+}
+
+public class Solution1 {
+
+    public static void main(String[] args) {
+
+        Student obj=new Student();
+        obj.sleep();
+        
+        
+
+    }
+    
+
+}
