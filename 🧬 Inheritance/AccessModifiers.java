d8e8bd5ccf9e5c2b24bf9421b206eb85a075bdf2
ADV Indian coder <vinaykumar860964@gmail.com>
2024-03-04 21:31:46 +0530

    Updated

---
+++ 🧬 Inheritance/AccessModifiers.java
@@ -0,0 +1,22 @@
+public class AccessModifiers {
+
+    public static void main(String[] args) {
+
+        // in java there are 4 types of access modifiers
+        // 1. public
+        // 2. private
+        // 3. protected
+        // 4. default
+
+        // public: if we use public access modifier then we can access the method or
+        // variable from anywhere in the program
+        // private: if we use private access modifier then we can access the method or
+        // variable only in the same class.
+        // protected: if we use protected access modifier then we can access the method
+        // or variable in the same package and in the sub class of the same package.
+        // default: if we don't use any access modifier then it will be default and
+        // then we can access the method or variable in the same package.
+
+        System.out.println("Access Modifiers");
+    }
+}
