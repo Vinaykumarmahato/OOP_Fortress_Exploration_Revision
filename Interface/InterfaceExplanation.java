1f931854ef497517e25fb95ab79d3c36bf7d8dd0
Vinay kumar <vinaykumar860964@gmail.com>
2024-03-07 16:17:22 +0530

    Updated

---
+++ Interface/InterfaceExplanation.java
@@ -0,0 +1,24 @@
+package Interface;
+
+/* Interface is similar to the abstract method which is used to achieve 100% Abstraction
+ * we can't create a object of interface in interface.
+ * No constructor in interface
+ * 
+ */
+
+interface A {
+
+   public static final int num = 20; //  by default it will be public static final 
+
+    void show();
+
+}
+
+public class InterfaceExplanation {
+    public static void main(String[] args) {
+
+        System.out.println(A.num);
+
+    }
+
+}
