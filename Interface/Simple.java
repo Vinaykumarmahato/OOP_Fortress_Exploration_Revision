35c1e3ede0d5068fb9528d842b6949c08323dc2f
ADV Indian coder <vinaykumar860964@gmail.com>
2024-03-07 19:17:10 +0530

    Updated

---
+++ Interface/Simple.java
@@ -0,0 +1,15 @@
+
+interface Vinay{
+
+    int a=10;
+    void show();
+}
+public class Simple {
+    
+    public static void main(String[] args) {
+
+        System.out.println(Vinay.a);
+        
+
+    }
+}
