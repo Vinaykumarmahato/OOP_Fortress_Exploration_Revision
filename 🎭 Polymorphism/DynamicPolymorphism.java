01fda8b866283a24bd2cd35bd9162933217f86d0
Vinay kumar <vinaykumar860964@gmail.com>
2024-03-07 13:53:59 +0530

    Updated

---
+++ 🎭 Polymorphism/DynamicPolymorphism.java
@@ -0,0 +1,10 @@
+
+public class DynamicPolymorphism {
+
+    // method overrriding it will be here
+    public static void main(String[] args) {
+        
+
+    }
+
+}
---
+++ 🎭 Polymorphism/StaticPolymorphism.java
@@ -0,0 +1,11 @@
+
+class Aeroplane{
+    
+}
+
+public class StaticPolymorphism {
+    // it direct refering the method overloading/ compile time polymorphism/
+    public static void main(String[] args) {
+        
+    }
+}
