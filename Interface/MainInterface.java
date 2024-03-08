2ffaf27724db19ebed9b95217965d9d0633a7eb6
Vinay kumar <vinaykumar860964@gmail.com>
2024-03-08 09:41:33 +0530

    Updated

--- Interface/MainInterface.java
+++ Interface/MainInterface.java
@@ -1,4 +1,9 @@
-class Laptop {
+interface Computer {
+    public void CompileCode();
+
+}
+
+class Laptop implements Computer {
 
     public void CompileCode() {
 
@@ -6,13 +11,20 @@ class Laptop {
     }
 }
 
-class Developer {
-    Laptop obj = new Laptop();
+class DeskTop implements Computer {
 
-    public void BuildApp() {
+    public void CompileCode() {
+
+        System.out.println("Hey! Don't Do The Silly Error! It Will Be Fast!");
+    }
+}
+
+class Developer{
+
+    public void BuildApp(DeskTop obj) {
 
         System.out.println("Building the app!");
-        //CompileCode();
+        // CompileCode();
         obj.CompileCode();
     }
 
@@ -22,8 +34,20 @@ public class MainInterface {
 
     public static void main(String[] args) {
         Developer Dev = new Developer();
+        // Laptop obj = new Laptop();
 
-        Dev.BuildApp();
+        DeskTop obj = new DeskTop();
+
+        Dev.BuildApp(obj);
+
+        /*
+         * Interface is a point where two system meet and interact
+         * 
+         * Interface is a group of related method with Empty body
+         * 
+         * image you have TV and Human and you are controlling both the system with
+         * Button it means here button is working as a interface.
+         */
 
     }
 }
