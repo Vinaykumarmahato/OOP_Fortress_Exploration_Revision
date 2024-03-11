8ce834d0802216125a82a525d31e33b3be61cafa
ADV Indian coder <vinaykumar860964@gmail.com>
2024-03-11 18:42:36 +0530

    Updated

---
+++ Interface/Simple2.java
@@ -0,0 +1,14 @@
+interface MakeMoney{
+
+    int a=10;
+}
+
+public class Simple2 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println(MakeMoney.a);
+
+    }
+
+}
