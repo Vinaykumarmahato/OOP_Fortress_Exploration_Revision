d9c4dfd409ec62fc36a872b65bf3d5a3aefe2ff6
Vinay kumar <vinaykumar860964@gmail.com>
2024-03-11 10:19:45 +0530

    Updated

---
+++ ⚠️🛠️Exception Handling/ExceptionExplanation.java
@@ -0,0 +1,49 @@
+
+public class ExceptionExplanation {
+    /*
+     * Different Type of Error in java
+     * 
+     * (Syntax Error)
+     * (Logical Error)
+     * (Run Time Error)
+     */
+
+    public static void main(String[] args) {
+
+        /*
+         * (Syntax Error)
+         * 
+         * Syntax error here Due to Systemword which has written Small 'S' it will be
+         * capital Thats why here syntax error will be.
+         * system.out.println("Hello World");
+         * 
+         * 
+         * (Logical Error)
+         * 
+         * In Logical Error Syntax error will not be there everything will be fine but
+         * the problem will be like your Except result will not be visible.
+         * Num1=5;
+         * Num2=-5;
+         * Num3;
+         * Num3=Num1-Num2;
+         * System.out.println("Addition Of Two number is:"+Num3)
+         * 
+         * Here Logical error will be occur because you are saying the addition of two
+         * number but Logically you have written the '-' which will give you a Logical
+         * error.
+         * 
+         * (Run Time Error)
+         * 
+         * This is the error everything will Looks fine but when we will execute the
+         * code it will give the error that is called Run Time Error
+        
+        int Num1 = 5;
+        int Num2 = 0; Due to it this will give you an error and we should handle it.
+        int Num3;
+        Num3 = Num1 / Num2;
+        System.out.println("Division :" + Num3);
+        System.out.println("Bye");
+
+ */
+    }
+}
