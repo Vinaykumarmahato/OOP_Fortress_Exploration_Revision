33cc9d962d0c5df990a57242715cab56aff2df0a
ADV Indian coder <vinaykumar860964@gmail.com>
2024-03-11 22:55:10 +0530

    Updated

--- 🔄🧵Multithreading/JoinMethodAndIsAliveMethod.java
+++ 🔄🧵Multithreading/JoinMethodAndIsAliveMethod.java
@@ -1,39 +1,43 @@
 class Printing implements Runnable {
 
+    // The run method is overridden to define the thread's behavior
     public void run() {
-
-        
-            try {
-                for (int i = 0; i < 2; i++) {
-                    System.out.println("Education is the most powerful weapon which you can use to Change The world.");
+        try {
+            // Loop to print a message and sleep for 2 seconds, repeated twice
+            for (int i = 0; i < 2; i++) {
+                System.out.println("Education is the most powerful weapon which you can use to Change The world.");
                 Thread.sleep(2000);
-            } 
-            
             }
-            catch (InterruptedException e) {
-                System.out.println("Some Problem Occured in the Thread!");
+        } catch (InterruptedException e) {
+            // Handle InterruptedException if it occurs
+            System.out.println("Some Problem Occurred in the Thread!");
         }
     }
 }
 
 public class JoinMethodAndIsAliveMethod {
 
-    public static void main(String[] args)throws Exception {
-
+    public static void main(String[] args) throws Exception {
         System.out.println("Main Thread Started!");
 
-        Printing p=new Printing();
-        Thread t1=new Thread(p);
-        System.out.println(t1.isAlive());// (O/p False) it is used to check the Thread is Alive or Not.
+        // Creating an instance of the Printing class that implements Runnable
+        Printing p = new Printing();
 
+        // Creating a new thread associated with the Printing instance
+        Thread t1 = new Thread(p);
+
+        // Checking if the thread is alive before starting (should be false)
+        System.out.println(t1.isAlive()); // Output: false
+
+        // Starting the thread, now it should be alive
         t1.start();
-        System.out.println(t1.isAlive());// (O/p True) it is used to check the Thread is Alive or Not.
+        
+        // Checking if the thread is alive after starting (should be true)
+        System.out.println(t1.isAlive()); // Output: true
 
-        t1.join();// it used to Stop the Excution of the Main Thread until the
-        // t1 Thread is not Finished.
-
+        // Using join() to wait for the t1 thread to finish before proceeding with the main thread
+        t1.join();
 
         System.out.println("Main Thread Ended!");
-
     }
 }
