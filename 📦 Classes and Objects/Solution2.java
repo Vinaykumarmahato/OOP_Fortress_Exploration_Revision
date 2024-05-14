a87a1fdc7c89acc228e34c705633fd54d0bbdb89
Vinay kumar <vinaykumar860964@gmail.com>
2024-05-14 10:36:57 +0530

    Updated

---
+++ 📦 Classes and Objects/Solution2.java
@@ -0,0 +1,19 @@
+class Class_and_Object{
+
+    public void Method(){
+
+        System.out.println("hello world");
+    }
+}
+
+public class Solution2 {
+
+    public static void main(String[] args) {
+
+        Class_and_Object co=new Class_and_Object();
+
+        co.Method();
+        
+
+    }
+}
