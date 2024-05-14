d61c2aa0045a1a34ed0eea7d5460b4005adcd5ed
Vinay kumar <vinaykumar860964@gmail.com>
2024-05-14 12:05:03 +0530

    Updated

---
+++ 🧬 Inheritance/🧬👑Types of Inheritance/SingleLevelInheritance2.java
@@ -0,0 +1,19 @@
+class Animal{
+
+
+}
+class Dog extends Animal{
+
+
+}
+public class SingleLevelInheritance2 {
+    
+    public static void main(String[] args) {
+
+
+        Animal a=new Animal();
+        
+        
+
+    }
+}
