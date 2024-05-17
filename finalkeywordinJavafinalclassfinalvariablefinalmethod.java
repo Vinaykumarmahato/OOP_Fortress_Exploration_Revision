1d1498a0d5c17c5fc9fa9b8fbe77014109865eef
Vinay kumar <116486037+Vinaykumarmahato@users.noreply.github.com>
2024-05-17 11:42:12 +0530

    Create finalkeywordinJavafinalclassfinalvariablefinalmethod.java

---
+++ finalkeywordinJavafinalclassfinalvariablefinalmethod.java
@@ -0,0 +1,30 @@
+final class Animal{
+    void sleep(){
+        System.out.println("Animal is Sleeping........");
+    }
+}
+
+// class dog extends Animal{
+//     //agar aap chahte hai ki aapka class kisi aur class me inherit na ho koe aur usme participate na kare to aap class ko finel kar do.
+
+// }
+
+
+
+public class finalkeywordinJavafinalclassfinalvariablefinalmethod {
+
+    public static void main(String[] args) {
+        
+        /*
+         * final class: agar aap chahte hai ki aapka class kisi aur class me inherit na ho koe aur usme participate na kare to aap class ko finel kar do.
+         * final method: ye access ho sakta hai child class me but usko wo override nahi kar sakta hai.
+         * Final Variable: agar pahle aap variable ko kuchh value diye hai bad me final karne ke bad aap usko change karna chahte hai to ye aap nahi kar sakte hai.
+         * 
+         * 
+         * 
+         */
+
+
+    }
+    
+}
