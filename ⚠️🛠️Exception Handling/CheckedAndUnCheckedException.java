04ab2bb848e93b63c08d696d27034a4df5ed0800
Vinay kumar <116486037+Vinaykumarmahato@users.noreply.github.com>
2025-01-10 14:03:18 +0530

    Update CheckedAndUnCheckedException.java

--- ⚠️🛠️Exception Handling/CheckedAndUnCheckedException.java
+++ ⚠️🛠️Exception Handling/CheckedAndUnCheckedException.java
@@ -2,8 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class CheckedAndUnCheckedException {
-    
-    
+
     public static void main(String[] args) {
         
         
